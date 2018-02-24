e2afc50365603d352da0936514c6f5394cbeaa55
haojunnan <34651903+haojunnan@users.noreply.github.com>
2018-02-25 00:07:38 +0800

    Update function.py

--- function.py
+++ function.py
@@ -4,20 +4,22 @@ import sys
 from star import Star
 from random import randint
 def check_events():
-	"""鼠标相应事件"""
+	"""榧犳爣鐩稿簲浜嬩欢"""
 	for event in pygame.event.get():
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_ESCAPE:
 				sys.exit()
 		elif event.type == pygame.QUIT:
 			sys.exit()
+			
 def update_screen(al_setting,stars,screen):
-	"""刷新界面"""
+	"""鍒锋柊鐣岄潰"""
 	screen.fill(al_setting.bg_color)
 	stars.draw(screen)
 	pygame.display.flip()
+	
 def creat_star(al_setting,screen,star_num,stars,row_number):
-	"""创建星星"""
+	"""鍒涘缓鏄熸槦"""
 	a = randint(0,1)
 	if a == 1:	
 		star = Star(al_setting,screen)
@@ -27,12 +29,15 @@ def creat_star(al_setting,screen,star_num,stars,row_number):
 		star.y = star.rect.height + star.rect.height * 2 * row_number
 		star.rect.y = star.y
 		stars.add(star)
+		
 def star_rows(al_setting,screen):
-	"""计算有多少行星星"""
+	"""璁＄畻鏈夊灏戣鏄熸槦"""
 	star = Star(al_setting,screen)
 	rows = int(al_setting.screen_height / (star.rect.height * 2))
 	return rows
+
 def creat_stars(al_setting,screen,stars):
+	"""鍒涘缓鏄熸槦"""	
 	star = Star(al_setting,screen)
 	star_nums =int(al_setting.screen_width / (star.rect.width * 2))
 	row_numbers = star_rows(al_setting,screen)
